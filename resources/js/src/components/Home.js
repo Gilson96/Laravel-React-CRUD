00a9ea29e0f4cf9882ad8d6534a69173fed2d9dd
gilson <gilson@wstudent-79-160-25.solent.ac.uk>
2020-10-06 14:45:39 +0100

    Personalized layout with bootstrap

--- resources/js/src/components/Home.js
+++ resources/js/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return(
@@ -6,7 +7,7 @@ const Home = () => {
             <div class="card">
                 <div class="card-header">CRUD</div>
                 <div class="card-body">
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <Link to="/add" class="btn btn-primary">Go somewhere</Link>
                    <div className="table-responsive">
                        <table className="table table-striped" mt-4>
                             <thead>
@@ -14,8 +15,20 @@ const Home = () => {
                                     <th>ID</th>
                                     <th>Title</th>
                                     <th>Descreption</th>
+                                    <th>Action</th>
                                 </tr>
                             </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Sample Title</td>
+                                    <td>Sample Descreption</td>
+                                    <td>
+                                        <a href="#" className="btn btn-warning">EDIT</a>
+                                        <a href="#" className="btn btn-danger">DELETE</a>
+                                    </td>
+                                </tr>
+                            </tbody>
                        </table>
                    </div>
                 </div>
