1137842995e6d034f592d0f22483cd64b96f371a
gilson <gilson@wstudent-79-160-25.solent.ac.uk>
2020-10-06 15:40:42 +0100

    New feature for form that links to edit page

--- resources/js/src/components/Home.js
+++ resources/js/src/components/Home.js
@@ -3,38 +3,37 @@ import { Link } from 'react-router-dom';
 import AppContainer from './AppContainer';
 
 const Home = () => {
-    return(
+    return (
         <AppContainer
-        
             title="CRUD"
             Children
         >
-             <Link to="/add" class="btn btn-primary">Go somewhere</Link>
-                   <div className="table-responsive">
-                       <table className="table table-striped" mt-4>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Title</th>
-                                    <th>Descreption</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Sample Title</td>
-                                    <td>Sample Descreption</td>
-                                    <td>
-                                        <a href="#" className="btn btn-warning">EDIT</a>
-                                        <a href="#" className="btn btn-danger">DELETE</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                       </table>
-                   </div>
+            <Link to="/add" class="btn btn-primary">ADD POST</Link>
+            <div className="table-responsive">
+                <table className="table table-striped" mt-4>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Descreption</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Sample Title</td>
+                            <td>Sample Descreption</td>
+                            <td>
+                                <Link to="/edit/1" class="btn btn-warning">EDIT</Link>
+                                <a href="#" className="btn btn-danger">DELETE</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </AppContainer>
-                   
+
     );
 };
 
