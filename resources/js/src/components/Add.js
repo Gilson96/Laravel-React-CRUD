185532281aa39265decbcf62eb1a9bfe8c5467db
gilson <gilson@wstudent-79-160-25.solent.ac.uk>
2020-10-06 15:28:13 +0100

    Added a form to ease new user data

--- resources/js/src/components/Add.js
+++ resources/js/src/components/Add.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import AppContainer from './AppContainer'
 
 const Add = () => {
     return(
-        <div className="Add__container">
-            This is Add component
-        </div>
+        <AppContainer
+            title="ADD POST"
+        >
+            <form>
+                <div className="form-gruop">
+                    <label>Title</label>
+                    <input className="form-control" type="text" />
+                </div>
+                <div className="form-gruop">
+                    <label>Description</label>
+                    <textarea 
+                        className="form-control" 
+                        type="text">
+                    </textarea>
+                </div>
+                <div className="form-group">
+                    <button type="button" className="btn btn-success">ADD</button>
+                </div>
+
+            </form>
+        </AppContainer>
     );
 };
 
