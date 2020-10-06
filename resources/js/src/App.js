e0198452d7e7e92d0802ccfae603d83c9f6937c7
gilson <gilson@wstudent-79-160-25.solent.ac.uk>
2020-10-06 14:44:22 +0100

    Added some imports

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -4,17 +4,31 @@ import{
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from "react-router-dom";
 import Home from './components/Home'
+import Add from './components/Add'
+import Edit from './components/Edit'
+
+
+
 
 const App = () => {
     return(
         <Router className="App__container">
             <Switch>
+
                 <Route exact path="/">
                     <Home />
                 </Route>
+
+                <Route path="/add">
+                    <Add />
+                </Route>
+
+                <Route path="/edit/:id">
+                    <Edit />
+                </Route>
+
             </Switch>
         </Router>
     );
